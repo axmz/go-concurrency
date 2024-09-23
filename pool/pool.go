c847b143672db0702724ac280605fd5c77b0eebc
Alexandr Moroz <afmoroz@gmail.com>
2024-09-23 14:33:11 +0000

    pool

---
+++ pool/pool.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"sync"
+	"time"
+)
+
+func pool() {
+	var numCalcsCreated int
+	calcPool := &sync.Pool{
+		New: func() interface{} {
+			numCalcsCreated += 1
+			mem := make([]byte, 1024)
+			return &mem
+		},
+	}
+	// Seed the pool with 4KB
+	calcPool.Put(calcPool.New())
+	calcPool.Put(calcPool.New())
+	calcPool.Put(calcPool.New())
+	calcPool.Put(calcPool.New())
+	const numWorkers = 1024 * 1024
+	var wg sync.WaitGroup
+	wg.Add(numWorkers)
+	for i := numWorkers; i > 0; i-- {
+		go func() {
+			defer wg.Done()
+			mem := calcPool.Get().(*[]byte)
+			// time.Sleep(time.Millisecond * 10)
+			defer calcPool.Put(mem)
+			// Assume something interesting, but quick is being done with
+			// this memory.
+		}()
+	}
+	wg.Wait()
+	fmt.Printf("%d calculators were created.", numCalcsCreated)
+}
+
+// Service
+func connectToService() interface{} {
+	time.Sleep(1 * time.Second)
+	return struct{}{}
+}
+
+func warmServiceConnCache() *sync.Pool {
+	p := &sync.Pool{
+		New: connectToService,
+	}
+	for i := 0; i < 10; i++ {
+		p.Put(p.New())
+	}
+	return p
+}
+
+func startNetworkDaemon() *sync.WaitGroup {
+	fmt.Println("Start service")
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		connPool := warmServiceConnCache()
+		server, err := net.Listen("tcp", "localhost:8080")
+		if err != nil {
+			log.Fatalf("cannot listen: %v", err)
+		}
+		defer server.Close()
+		wg.Done()
+		for {
+			conn, err := server.Accept()
+			fmt.Println("Conn")
+			if err != nil {
+				log.Printf("cannot accept connection: %v", err)
+				continue
+			}
+			svcConn := connPool.Get()
+			fmt.Fprintln(conn, "")
+			connPool.Put(svcConn)
+			conn.Close()
+		}
+	}()
+	return &wg
+}
+
+func init() {
+	daemonStarted := startNetworkDaemon()
+	daemonStarted.Wait()
+}
+
+func main() {
+	// pool()
+}
